01fd9da92d40d0e84c404c71e24b7ad341cd7518
Claret15 <37543659+Claret15@users.noreply.github.com>
2018-11-16 02:12:39 +0000

    Add styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import TodoList from "./TodoList";
-import "./App.css";
+import styles from "./App.module.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>See Our Todos!</h1>
+        <h1 className={styles.heading}>Todo <span className={styles.headingBold}>List!</span></h1>
+        <h2 className={styles.subHeading}>A simple todo list app built with node</h2>
         <TodoList />
       </div>
     );
